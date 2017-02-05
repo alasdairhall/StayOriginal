ff02e0f005ca397c3370e984dfb0861d31343455
Alasdair Hall <Alasdair Hall>
2017-02-05 12:30:27 +0000

    mvp

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-var url = require('url');
 var express = require('express');
-var authenticator = require('./authenticator');
-var config = require('./config');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var url = require('url');
+
 var app = express();
 
-// Add cookie parsing functionality to our Express app
-app.use(require('cookie-parser')());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// app.set('views', __dirname)
-app.set('view engine', 'ejs') // register the template engine
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('views'));
+
+var authenticator = require('./authenticator');
+var config = require('./config');
 
 app.get('/', function(res, res) {
 	res.render('home');
@@ -31,16 +43,18 @@ app.get(url.parse(config.oauth_callback).path, function(req, res) {
 	});
 });
 
-app.get('/tweet', function(req, res) {
+app.post('/tweet', function(req, res) {
 	if(!req.cookies.access_token || !req.cookies.access_token_secret) {
 
 		return res.redirect('/auth/twitter');
 	}
 
+	console.log("req.body.tweet: " + req.body.tweet);
+
 	authenticator.post('https://api.twitter.com/1.1/statuses/update.json',
 		req.cookies.access_token, req.cookies.access_token_secret,
 		{
-			status: "Oi oi m8"
+			status: req.body.tweet
 		},
 		function(error, data) {
 			if (error) {
